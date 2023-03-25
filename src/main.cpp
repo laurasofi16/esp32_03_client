c164dbd4bf279523ef30bd3ec5b02f937dc6cc1c
Laura Sofia Gonzalez <laura.gonzalez02@uceva.edu.co>
2023-03-25 15:40:56 -0500

    modified

--- src/main.cpp
+++ src/main.cpp
@@ -1,117 +1,61 @@
 #include <Arduino.h>
 #include <WiFi.h>
- 
-const char * ssid = "lsgo";
+
+const char * nombre = "lsgo";
 const char * password = "1234abcd";
+const char * host ="www.google.com";
+const int puerto = 80;
 
 
-String getTipoCifrado(wifi_auth_mode_t tipoCifrado) {
- 
-  switch (tipoCifrado) {
-    case (WIFI_AUTH_OPEN):
-      return "Open";
-    case (WIFI_AUTH_WEP):
-      return "WEP";
-    case (WIFI_AUTH_WPA_PSK):
-      return "WPA_PSK";
-    case (WIFI_AUTH_WPA2_PSK):
-      return "WPA2_PSK";
-    case (WIFI_AUTH_WPA_WPA2_PSK):
-      return "WPA_WPA2_PSK";
-    case (WIFI_AUTH_WPA2_ENTERPRISE):
-      return "WPA2_ENTERPRISE";
-  }
-}
- 
-void scanNetworks() {
- 
-  int numberOfNetworks = WiFi.scanNetworks();
- 
-  Serial.print("Numero de redes encontradas: ");
-  Serial.println(numberOfNetworks);
- 
-  for (int i = 0; i < numberOfNetworks; i++) {
- 
-    Serial.print("Nombre de red: ");
-    Serial.println(WiFi.SSID(i));
- 
-    Serial.print("Fuerza de la señal: ");
-    Serial.println(WiFi.RSSI(i));
- 
-    Serial.print("Direccion MAC: ");
-    Serial.println(WiFi.BSSIDstr(i));
- 
-    Serial.print("Tipo de cifrado: ");
-    String tipoCifradoDescription = getTipoCifrado(WiFi.encryptionType(i));
-    Serial.println(tipoCifradoDescription);
-    Serial.println("-----------------------");
- 
-  }
-}
- 
-void connectToNetwork() {
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Estableciendo conexion WiFi..");
-  }
- 
-  Serial.println("Conectado a la red");
- 
-}
- 
 void setup() {
- 
-  Serial.begin(9600);
- 
-  scanNetworks(); //Escanear las redes WiFi disponibles
-  connectToNetwork();  //Conectarse a la red que especificamos arriba
- 
-  Serial.println(WiFi.macAddress());
-  Serial.println(WiFi.localIP());
- 
-//  WiFi.disconnect(true);
-//  Serial.println(WiFi.localIP());
- 
+
+  pinMode(2,OUTPUT); // coloco el pin 2 como salida
+  Serial.begin(115200);//confuguracion de velocidad de wifi
+  Serial.println("Inicializado el dispositivo");
+  WiFi.begin(nombre,password);
+
+  Serial.printf("Estableciendo vinculo con el AP");
+  while (WiFi.status() !=WL_CONNECTED){
+  Serial.print(".");
+  delay(1000);
+    
+  }
+  Serial.println("\r\n Conexion establecida");
+  Serial.println("numero IP asignado");
+  Serial.println(WiFi.localIP());//imprime el numero ip
+  Serial.println();
 }
- 
+
+
 void loop() {
-   Serial.println("Conectandose a:");
-   Serial.println(host);
-
-   WiFiClient client;
-
-   const int httpPort = 80;
-   
-   if(!client.connect(host, httpPort)){
-     Serial.println("Conexion Fallida");
-     delay(1000);
-     return;
-   }
-
-  String url = "/index.html";
-
-  Serial.print("Solicitando el recurso: ");
-  Serial.println(url);
-
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" 
-                    + "Connection: close\r\n\r\n");
-  unsigned long timeout = millis(); //Leo la hora
-  while(client.available() == 0){   //Mientras hayan cero letras disponibles...
-      if(millis() - timeout > 5000){ //Resto la hora actual a la hora inicial, si es mas de 5s
-        Serial.println("Expiro el tiempo de espera");
-        client.stop();
-        return;
-      }
+  WiFiClient Cliente;
+  //
+  if(!(Cliente.connect(host,puerto))){
+    Serial.println("Error, conexion al host fallida");
+    delay(3000);
+    return;
   }
-
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.println(line);
-  }
+  Serial.print("conexion exitosa al host ");
   
-  Serial.println("Fin de conexion");
-  client.stop();
+  // peticion GET al servidor http
+  Cliente.print("GET /index.html HTTP/1.1\r\nHost: "+String(host)+"\r\n"+"Connection: close\r\n\r\n");
+
+  //Debemos darle un tiempo al servidor a que responda la peticion
+  //delay(5000); // no funciona aqui bloquea el wifi 
+  unsigned long milisegundos=millis(); //hora de inicio
+
+while(Cliente.available()==0){// preguntamos si no hay datos recibidos disponibles
+    if(millis()-milisegundos>7000){
+      Serial.println("se expiro el tiempo de la conexion");
+      Cliente.stop();
+    }
 }
 
+while (Cliente.available()){
+  String linea = Cliente.readStringUntil('\r');
+  Serial.println(linea);
+}
+
+  Serial.println("Fin de la conexion");
+  Cliente.stop();
+  delay(2000);
